d098e564073da75f3cf33b3061b176e6b174a029
Isuru Lakshan <e16203@eng.pdn.ac.lk>
2022-03-04 14:39:44 +0530

    logger routes added

--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,19 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
 
     Route::get('/faculty/create', [App\Http\Controllers\FacultyController::class, 'create']);
 });
+
+Route::group(['prefix' => 'activity', 'namespace' => 'App\Http\Controllers', 'middleware' => ['web', 'auth', 'activity']], function () {
+
+    // Dashboards
+    Route::get('/', 'LaravelLoggerController@showAccessLog')->name('activity');
+    Route::get('/cleared', ['uses' => 'LaravelLoggerController@showClearedActivityLog'])->name('cleared');
+
+    // Drill Downs
+    Route::get('/log/{id}', 'LaravelLoggerController@showAccessLogEntry');
+    Route::get('/cleared/log/{id}', 'LaravelLoggerController@showClearedAccessLogEntry');
+
+    // Forms
+    Route::get('/clear-activity', ['uses' => 'LaravelLoggerController@clearActivityLog'])->name('clear-activity');
+    Route::delete('/destroy-activity', ['uses' => 'LaravelLoggerController@destroyActivityLog'])->name('destroy-activity');
+    Route::post('/restore-log', ['uses' => 'LaravelLoggerController@restoreClearedActivityLog'])->name('restore-activity');
+});
