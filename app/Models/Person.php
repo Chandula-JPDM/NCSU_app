ef7587e7c7eb0182ec7a1742950713e07d9f0469
chandula <e16061@eng.pdn.ac.lk>
2022-03-09 22:46:08 +0530

    add hidden fields

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -32,6 +32,25 @@ class Person extends Authenticatable
         'department_id',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function faculty(){
         return $this->belongsTo(Faculty::class);
     }
